8ab4dcdd92de203424bc9108892c5cb4ae40b3b2
Rahul <rahul.itechca@gmail.com>
2025-11-20 15:41:46 -0500

    admin folder created

--- reusable/header/nav.php
+++ reusable/header/nav.php
@@ -7,9 +7,9 @@
 
 
 <header class="d-flex align-items-center justify-content-between">
-    <h2><a href="/">Parks and Recreation</a></h2>
+    <h2><a href="/assignment2/assignment2">Parks and Recreation</a></h2>
     <ul class="nav" >
-        <li><a href="/">Register</a></li>
+        <li><a href="register.php">Register</a></li>
         <li><a href="/">Login</a></li>
     </ul>
 </header>
