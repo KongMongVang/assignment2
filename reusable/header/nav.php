ff6526c384796e1bcbff7472bdcfe77ebf030168
Kong Mong Vang <vangkong@icloud.com>
2025-11-20 16:25:11 -0500

    Add changed

--- reusable/header/nav.php
+++ reusable/header/nav.php
@@ -7,9 +7,14 @@
 
 
 <header class="d-flex align-items-center justify-content-between">
+<<<<<<< Updated upstream
     <h2><a href="/assignment2/assignment2">Parks and Recreation</a></h2>
+=======
+    <h2><a href="/">Parks and Recreation</a></h2>
+
+>>>>>>> Stashed changes
     <ul class="nav" >
         <li><a href="register.php">Register</a></li>
         <li><a href="/">Login</a></li>
     </ul>
-</header>
+</header>   
