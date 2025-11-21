264d07994757c38f91aab06379e59e8a0882c008
Chenxi Lin <lin.chenxi14@gmail.com>
2025-11-20 21:37:57 -0500

    Add styling to header

--- register.php
+++ register.php
@@ -51,7 +51,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
     <title>Register</title>
+<<<<<<< Updated upstream
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+=======
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+>>>>>>> Stashed changes
 
 <style>
 :root {
